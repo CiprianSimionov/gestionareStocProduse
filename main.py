9a6586d7267472f04043f383d14b6552a0f1ca55
ciprian simionov <simionov.ciprian@gmail.com>
2024-10-29 17:38:29 +0200

    implementing and running on main.py

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
-"""cream metodele pentru ca aplicatia sa fie interactiva"""
+"""Creating models for app interactions"""
 from db.db_connection import create_database
 from models.products import Product
 from db.crud.products_crud import ProductsDB
+from models.users import User
+from db.crud.users_crud import UsersDB
 
-#functiile pentru user
 
-def add_user(username, first_name, last_name, email, password, confirm_password):
-    pass
 
-def update_user(username, first_name, last_name, email, password):
-    pass
 
-def delete_user(id_user):
-    pass
+#user functions
+
+def add_user(username, first_name, last_name, email, password):
+
+        try:
+            new_user = User(username=username, first_name=first_name, last_name=last_name, email=email, password=password)
+            new_user.add()
+            print("User added successfully into the system")
+        except Exception as e:
+            print(f"Following error adding user: {e}")
+
+def update_user(user_id, username, first_name, last_name, email, password):
+    try:
+        user_updated = User(id=user_id, username=username, first_name=first_name, last_name=last_name,
+                                  email=email, password=password)
+        user_updated.update()
+        print("User updated.")
+    except Exception as e:
+        print(f"Error is: {e}")
+
+def delete_user(user_id):
+    try:
+        deleted_user = User(id=user_id, username="", first_name="", last_name="", email="", password="")
+        deleted_user.delete_user()
+        print("User deleted.")
+    except Exception as e:
+        print(f"Error is: {e}")
 
 def all_users():
-    pass
+    users_db = UsersDB()
+    users = users_db.read()
+    for user in users:
+        print(f"Id: {user['id']}, username: {user['username']}, email: {user['email']}")
 
 def get_info_user():
     username = input("Insert username: ")
@@ -28,46 +53,47 @@ def get_info_user():
 def get_info_user_id():
     return input("Insert your id: ")
 
-#functii pentru products
+
+#product functions
 
 def add_product(name, description, ingredients, price, weight, quantity):
     try:
-        produs_nou = Product(product_name=name, description=description, ingredients=ingredients, price=price, weight=weight, quantity=quantity)
-        produs_nou.add()
-        print("Produs adaugat cu succes in sistem")
+        new_product = Product(product_name=name, description=description, ingredients=ingredients, price=price, weight=weight, quantity=quantity)
+        new_product.add()
+        print("Product added successfully into the system")
     except Exception as e:
-        print(f"Eroarea la adaugarea produsului este: {e}")
+        print(f"Following error adding product: {e}")
 
 def update_product(product_id, name, description, ingredients, price, weight, quantity):
     try:
-        produs_actualizat = Product(id=product_id, product_name=name, description=description, ingredients=ingredients, price=price, weight=weight, quantity=quantity)
-        produs_actualizat.update()
-        print("Produsul a fost actualizat.")
+        product_updated = Product(id=product_id, product_name=name, description=description, ingredients=ingredients, price=price, weight=weight, quantity=quantity)
+        product_updated.update()
+        print("Product updated.")
     except Exception as e:
-        print(f"Eroarea este: {e}")
+        print(f"Error is: {e}")
 
 
 def delete_product(product_id):
     try:
         produs_sters = Product(id=product_id, product_name="", description="", ingredients="", price=None, weight=None, quantity=None)
         produs_sters.delete()
-        print("Produsul a fost sters din sistem.")
+        print("Product deleted.")
     except Exception as e:
-        print(f"Eroarea este: {e}")
+        print(f"Error is: {e}")
 
 def all_products():
     products_db = ProductsDB()
-    produse = products_db.read()
-    for produs in produse:
-        print(f"Id: {produs['id']}, nume_produs: {produs['product_name']}")
+    products = products_db.read()
+    for product in products:
+        print(f"Id: {product['id']}, product_name: {product['product_name']}, product_price: {product['price']}")
 
 def get_info_product():
     name = input("Insert product name: ")
     description = input("Insert product description: ")
     ingredients = input("Insert ingredients: ")
     price = float(input("Insert product price: "))
-    weight = int(input("Insert weight: "))
-    quantity = int(input("Insert the quantity: "))
+    weight = int(input("Insert product weight(integer): "))
+    quantity = int(input("Insert stock quantity: "))
     return name, description, ingredients, price, weight, quantity
 
 def get_info_product_id():
@@ -75,16 +101,47 @@ def get_info_product_id():
 
 if __name__ == "__main__":
     create_database()
-    print("Ce date vrei sa introduci in sistem? ")
-    print("1. utilizatori")
-    print("2. produse")
-    optiune = input("Scrie una din cele 2 optiuni: ")
+    print("What data would you like to enter into the system? ")
+    print("1. Users")
+    print("2. Products")
+    option = input("Select one option: ")
 
-    if optiune == "1":
-        print("Pentru utilizatori poti face urmatoarele operatiuni: ")
+    if option == "1":
+        print("For users you have following operations: ")
+        while True:
+            print("1. Add user: ")
+            print("2. Update user ")
+            print("3. Delete user: ")
+            print("4. List user: ")
+            print("5. Exit")
 
-    elif optiune == "2":
-        print("Pentru produse poti face urmatoarele operatiuni: ")
+            option = input("Select option: ")
+
+            if option == "1":
+                username, first_name, last_name, email, password = get_info_user()
+                add_user(username, first_name, last_name, email, password)
+                #print("User added.")
+            elif option == "2":
+                user_id = get_info_user_id()
+                username, first_name, last_name, email, password = get_info_user()
+                update_user(user_id, username, first_name, last_name, email, password)
+                #print("User updated.")
+            elif option == "3":
+                user_id = get_info_user_id()
+                delete_user(user_id)
+                #print("User deleted.")
+            elif option == "4":
+                all_users()
+                #print("List of available users.")
+            elif option == "5":
+                break
+
+            else:
+                print("Invalid option.Try again")
+
+
+    elif option == "2":
+        print("For products you have following operations: ")
         while True:
             print("1. Add product: ")
             print("2. Update product: ")
@@ -92,32 +149,31 @@ if __name__ == "__main__":
             print("4. List products: ")
             print("5. Exit")
 
-            optiune = input("Scrie numarul operatiunii: ")
+            option = input("Select option: ")
 
-            if optiune == "1":
-
+            if option == "1":
                 name, description, ingredients, price, weight, quantity = get_info_product()
                 add_product(name, description, ingredients, price, weight, quantity)
-                print("Am adaugat produsul.")
-            elif optiune == "2":
+                #print("Product added.")
+            elif option == "2":
                 product_id = get_info_product_id()
                 name, description, ingredients, price, weight, quantity = get_info_product()
                 update_product(product_id, name, description, ingredients, price, weight, quantity)
-                print("Am actualizat produsul.")
-            elif optiune == "3":
+                #print("Product updated.")
+            elif option == "3":
                 product_id = get_info_product_id()
                 delete_product(product_id)
-                print("Am sters produsul.")
-            elif optiune == "4":
+                #print("Product deleted.")
+            elif option == "4":
                 all_products()
-                print("Lista produse: ")
-            elif optiune == "5":
+                #print("Products list: ")
+            elif option == "5":
                 break
             else:
-                print("Nu exista aceasta optiune.Incearca din nou")
+                print("Invalid option. Try again.")
 
     else:
-        print("Nu exista aceasta optiune. Incearca din nou.")
+        print("Invalid option. Try again..")
 
 
 
