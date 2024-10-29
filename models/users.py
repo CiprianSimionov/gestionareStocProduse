074dc27256517462579023356ac7490960b620ad
ciprian simionov <simionov.ciprian@gmail.com>
2024-10-29 17:27:39 +0200

    updating user model

--- models/users.py
+++ models/users.py
@@ -1,11 +1,13 @@
-"""cream modelul(obiectul) pentru aplicatia noastra(gestionare stoc produse"""
+"""Create model(object) for current application"""
 
-from db.crud.users_crud import  UsersDB
+from db.crud.users_crud import UsersDB
+import uuid
+
 
 class User:
 
-    def __init__(self, username, first_name, last_name, email, password, confirm_password = None, id = None):
-        self.id = id
+    def __init__(self, username, first_name, last_name, email, password, confirm_password=None, id=None):
+        self.id = id or str(uuid.uuid4())
         self.username = username
         self.first_name = first_name
         self.last_name = last_name
@@ -19,11 +21,13 @@ class User:
         self.user_db.create(self.dict_create())
 
     def update(self):
-        pass
+        user_data = self.dict_create()
+        with UsersDB() as db:
+            db.update(user_data, self.id)
 
     def delete_user(self):
         if not self.id:
-            raise Exception("Avem nevoie de id pentru a sterge utilizatorul.")
+            raise Exception("Id required in order to delete user.")
         self.user_db.delete(self.id)
 
     def dict_create(self):
